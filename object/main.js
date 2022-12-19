be5a2581536c05c0db9c4f5b87eca56c011b6b25
Admin <kavyak6999@gmail.com>
2022-12-19 14:18:21 +0530

    object-1

---
+++ object/main.js
@@ -0,0 +1,10 @@
+//a = document.getElementById("america");
+//console.log(a);
+
+//a = document.getElementsByClassName("america");
+//console.log(a);
+
+a = document.getElementById("america");
+console.log(a);
+a.innerHTML="vanakam";
+
